272edd12cf1da29f079d5d5bf396f3da06695864
wafu-alt <rhfemtlfem@naver.com>
2022-03-26 17:52:23 +0900

    Update test.js

--- BAEKJOON/test.js
+++ BAEKJOON/test.js
@@ -1,41 +1,5 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\r\n");
+let input = fs.readFileSync(filePath).toString();
 
-//console.log(input);
-
-let cirNum = parseInt(input[0]);
-
-let arry = [];
-let str;
-for (i = 1; i <= cirNum; i++) {
-  arry = input[i].split(" ");
-  str = arry[1];
-  let strArr = [];
-  //console.log(str);
-  for (j = 0; j < str.length; j++) {
-    for (k = 0; k < arry[0]; k++) {
-      strArr += str[j];
-    }
-  }
-  console.log(strArr);
-}
-//console.log(arry);
-
-/*
-const num1 = input.shift();
-
-for (let i = 0; i < num1; i++) {
-    let answer = '';
-    
-    const [num2, str] = input[i].split(" ");
-    
-    for (let j = 0; j < str.length; j++) {
-        for (let c = 0; c < num2; c++) {
-            answer += str[j];
-  	}
-    }
-    
-    console.log(answer);
-}
-*/
+console.log(input);
