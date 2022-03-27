94dad6282053b9b1d630b770bb71939d08cd2830
wafu-alt <rhfemtlfem@naver.com>
2022-03-27 17:16:58 +0900

    Update test.js

--- BAEKJOON/test.js
+++ BAEKJOON/test.js
@@ -3,3 +3,24 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString();
 
 console.log(input);
+/*
+const result = new Array(26).fill(0);
+
+for (let i = 0; i < input.length; i++) {
+  result[input.charCodeAt(i) - 97] ++;
+}
+
+const max = Math.max(...result);
+const index = result.indexOf(max);
+
+let isSame = false;
+
+for (let j = 0; j < 26; j++) {
+  if (result[j] === max && index != j) {
+    isSame = true;
+    break;
+  }
+}
+
+console.log(isSame ? "?" : String.fromCharCode(index + 65));
+*/
